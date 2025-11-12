7132a9e29e35d6af5bd58befd6a8ad8308ebeb7b
ahp002 <aliciap.ap6@gmail.com>
2025-11-11 23:44:39 -0800

    removed browsers auto validation and did basic skeleton for name validation error msg popup

--- form-with-js.js
+++ form-with-js.js
@@ -14,17 +14,34 @@ function showError(input, message) {
 
 function enforceCharacterRules(event) {
     const input = event.target;
+    input.setCustomValidity("");
     if (!allowedChars1.test(input.value)) {
         showError(input, "Invalid character entered.");
         input.value = input.value.replace(/[^A-Za-z\s]/g, "");
     } else {
         input.classList.remove("flash");
+        input.setCustomValidity("");
     }
 }
 
 const nameInput = document.getElementById("name");
 nameInput.addEventListener("input", enforceCharacterRules);
 
-nameInput.addEventListener("invalid", (e) => {
-    showError(nameInput, "Name must be at least 4 characters. Valid characters include A-z.");
-})
+const form = document.getElementById("form");
+function validateName() {
+    nameInput.setCustomValidity("");
+    if (!nameInput.checkValidity()) {
+        nameInput.setCustomValidity("Name cannot be blank.");
+        e.preventDefault(); 
+        return false;
+    }
+    else if (nameInput.validity.tooShort) {
+        nameInput.setCustomValidity("Name must be at least 4 characters.");
+        e.preventDefault(); 
+        return false;
+    }
+    nameInput.setCustomValidity("");
+    return true;
+}
+
+form.addEventListener("Submit", validateName());
