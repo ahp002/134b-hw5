6706a26fa04e605e30a6bf0e3cf871a61d3b2b93
ahp002 <aliciap.ap6@gmail.com>
2025-12-01 22:29:26 -0800

    started styling individual portfolio card

---
+++ projects-css.js
@@ -0,0 +1,12 @@
+export function cardCss() {
+    return `
+        .project-card {
+            
+            display: flex;
+            flex-direction: column;
+            width: 300px;
+            height: 350px;
+            transition: 0.3s;
+        }
+    `;
+}
