3632f9f48eb8e6f42359cc34fce0a35fb597f789
ahp002 <aliciap.ap6@gmail.com>
2025-12-03 01:57:59 -0800

    create button and form for new <project-card> creation

--- crud.js
+++ crud.js
@@ -1,6 +1,21 @@
-function createCard() {
+// create a card
+let projects = JSON.parse(localStorage.getItem('projects')) || [];
+document.getElementById('createForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newProject = {
+    img: document.getElementById('create-img').value.trim(),
+    alt: document.getElementById('create-alt').value.trim(),
+    title: document.getElementById('create-title').value.trim(),
+    description: document.getElementById('create-description').value.trim(),
+    github: document.getElementById('create-github').value.trim(),
+    website: document.getElementById('create-website').value.trim()
+    };
+    projects.push(newProject);
+    localStorage.setItem('projects', JSON.stringify(projects));
+    e.target.reset();
+});
 
-}
+
 
 function updateCard() {
 
@@ -9,3 +24,5 @@ function updateCard() {
 function deleteCard() {
 
 }
+
+// list of project cards currently in local storage
